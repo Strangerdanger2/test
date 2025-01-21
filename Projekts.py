2bc25a2b0d49c7766f26756c7e63a3e51fa2b6ac
Strangerdanger2 <matissgraud1892@gmail.com>
2025-01-21 10:41:17 +0200

    Pa jaunu uztaisīju kodu

--- Projekts.py
+++ Projekts.py
@@ -1,58 +1,36 @@
-# Šis kods aprēķinās cik ilgs laiks ir vajadzīgs lai noskatītos kādu šovu.
+# Programma kas aprekina cik ilgs laiks ir nepieciesams lai noskatitos kadu sovu
 
-# Definīcijas
-from datetime import datetime,timedelta
 import math
 a = 0
-b = 0
-list=[]
+list = []
 
-
-# Input
+# Ievaddati
 
 nosaukums=str(input('Ievadiet šova nosaukumu: '))
-
 sez=int(input('Cik sezonu ir šim šovam? '))
 
-    # Cikls kas saglabā cik episožu ir katrā sezonā.
+# Cikls kas saglabā cik episožu ir katrā sezonā.
 while a < sez:
     a += 1
     item=int(input(f'Ievadiet episožu sk {a}. sezonā. '))
     list.append(item)
 
 ep = sum(list)
+ep_gar_dic={'īsa':20, 'vidēji īsa':30, 'vidēja':40, 'vidēji gara':50, 'gara': 60}
+ep_lenght=(input('Ievadiet episodes garumu (Īsa (20min), vidēji īsa (30 min), vidēja(40 min), vidēji gara (50 min), gara (60 min)): ').lower())
+watch_min=int(input('Cik min dienā jūs varat skatīties? '))
+koef=float(input('Ievadiet skatīšanās laika koef (x0.5, x1, x1.5, x2) '))
+#break_between_ep=int('Ievadiet pauzi starp epizodēm. Piem.(10, 15, 20 min) ')
 
-ep_gar={'īsa':20, 'vidēji īsa':40, 'vidēja':40, 'vidēji gara':50, 'gara': 60}
+# Aprēķins
 
-gar=(input('Ievadiet episodes garumu (Īsa (20min), vidēji īsa (30 min), vidēja(40 min), vidēji gara (50 min), gara (60 min)): ').lower())
+# Cik min skatīsies. Man vajag iziet cauri sezonam un iznemt epizodes lai es varetu aprekinat laiku.
+total_time_min = (ep * ep_gar_dic[ep_lenght]) // koef        #Aprēķina cik min paies lai noskatitos visu sovu, ar koeficentu
+diena = total_time_min // watch_min                          #Cik dienas skatīsies
+leftover_min = (total_time_min - (diena * watch_min))        #Cik minutēs palika kuras nevarēja pārveidot uz pilnām dienām
+reserve_h = leftover_min // 60                               #Atlikušās stundas no rezerves min
+min = leftover_min % 60                                      #Atlikušās min, pēc stundas aprēķina
+    
 
-koef=float(input('Ievadiet atskaņošanas ātruma koeficentu (1x, 1.5x, 2x) '))
-
-time,pauz=map(int,input('Pēc cik ilga skatīšanās laika jūs ņemsiet pauzi(A) un cik ilgi(B)? (Rakstiet formā: A B)').split())
-
-t_day=int(input('Cik min dienā jūs varat skatīties? '))
-
-
-# Aprēķins.
-laiks_bez_pauz=(ep_gar[gar] * koef) * float(ep)    # Aprēķina cik minūtes aizņēms šis šovs.
-print(laiks_bez_pauz)
-
-if time == 0:
-    kop_laiks=laiks_bez_pauz
-else:
-    kop_laiks=(laiks_bez_pauz//time) * pauz + laiks_bez_pauz                  # Aprēķina cik būs kopēja pauze.
-    b = (laiks_bez_pauz//time) * pauz
-    print(kop_laiks, laiks_bez_pauz, b)
-
-
-
-stundas=kop_laiks/60                                     # Pārveido kopējās min, uz stundām.
-dienas=laiks_bez_pauz/t_day                              # Pārveido no stundām uz dienām.
-atlikums=stundas%24                                      # Cik stundas paliek pēc dienām.
-
-#datetime
-now=datetime.now()
-end_t= now + timedelta (hours=atlikums, days=dienas)
-
-# Izvade.
-print(f'Jūs šovu {nosaukums} noskatīsīties {stundas:.0f}h, jeb {dienas:.0f} Dienās, {stundas%24:.0f}h. Jūs šo šovu pabeigsiet skatīties: {end_t}')  # Aprēķina cik ilgs būs kopējais skatīšanās laiks. Ieskaitot pauzes.
+# Izvade
+print(f'Jūs šovu {nosaukums}, skatīsieties: Kopējās min {total_time_min:.0f}, Cik dienas tas aizņems {diena:.0f}, palikušās min {leftover_min:.0f}, Cik pilnu stundu palika {reserve_h:.0f}, cik min palika {min:.0f}')
