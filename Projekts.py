8c24fbfc5c1afb83b42139dcb4e1585b7f3bf873
Strangerdanger2 <matissgraud1892@gmail.com>
2025-01-21 11:37:06 +0200

    Uzlaboju kodu pēc uzdevuma prasībām

--- Projekts.py
+++ Projekts.py
@@ -1,35 +1,45 @@
-# Programma kas aprekina cik ilgs laiks ir nepieciesams lai noskatitos kadu sovu
-
 import math
-a = 0
-list = []
 
-# Ievaddati
+def ievaddati():
+    nosaukums = str(input('Ievadiet šova nosaukumu: '))
+    sez = int(input('Cik sezonu ir šim šovam? '))
+    return nosaukums, sez
 
-nosaukums=str(input('Ievadiet šova nosaukumu: '))
-sez=int(input('Cik sezonu ir šim šovam? '))
+def ievadi_ep_skaitu(sez):
+    epizodes_skaits = []
+    for a in range(1, sez + 1):
+        item = int(input(f'Ievadiet epizožu sk {a}. sezonā. '))
+        epizodes_skaits.append(item)
+    return epizodes_skaits
 
-# Cikls kas saglabā cik episožu ir katrā sezonā.
-while a < sez:
-    a += 1
-    item=int(input(f'Ievadiet epizožu sk {a}. sezonā. '))
-    list.append(item)
+def ievadi_papildus_datus():
+    ep_gar_dic = {'īsa': 20, 'vidēji īsa': 30, 'vidēja': 40, 'vidēji gara': 50, 'gara': 60}
+    ep_lenght = input('Ievadiet epizodes garumu (Īsa (20min), vidēji īsa (30 min), vidēja(40 min), vidēji gara (50 min), gara (60 min)): ').lower()
+    watch_min = int(input('Cik min dienā jūs varat skatīties? '))
+    koef = float(input('Ievadiet skatīšanās laika koef (x0.5, x1, x1.5, x2) '))
+    return ep_gar_dic, ep_lenght, watch_min, koef
 
-ep = sum(list)
-ep_gar_dic={'īsa':20, 'vidēji īsa':30, 'vidēja':40, 'vidēji gara':50, 'gara': 60}
-ep_lenght=(input('Ievadiet epizodes garumu (Īsa (20min), vidēji īsa (30 min), vidēja(40 min), vidēji gara (50 min), gara (60 min)): ').lower())
-watch_min=int(input('Cik min dienā jūs varat skatīties? '))
-koef=float(input('Ievadiet skatīšanās laika koef (x0.5, x1, x1.5, x2) '))
+def aprekins(ep, ep_gar_dic, ep_lenght, watch_min, koef):
+    total_time_min = (ep * ep_gar_dic[ep_lenght]) // koef
+    diena = total_time_min // watch_min
+    leftover_min = int(total_time_min - (diena * watch_min))
+    reserve_h = leftover_min // 60
+    min = leftover_min % 60
+    return total_time_min, diena, leftover_min, reserve_h, min
 
-# Aprēķins
+def izvade(nosaukums, total_time_min, diena, leftover_min, reserve_h, min):
+    print(f'Jūs šovu {nosaukums}, skatīsieties: Kopējās min {total_time_min:.0f}, '
+          f'Cik dienas tas aizņems {diena:.0f}, palikušās min {leftover_min:.0f}, '
+          f'Cik pilnu stundu palika {reserve_h:.0f}, cik min palika {min:.0f}')
 
-# Cik min skatīsies. Man vajag iziet cauri sezonam un iznemt epizodes lai es varetu aprekinat laiku.
-total_time_min = (ep * ep_gar_dic[ep_lenght]) // koef        #Aprēķina cik min paies lai noskatitos visu sovu, ar koeficentu
-diena = total_time_min // watch_min                          #Cik dienas skatīsies
-leftover_min = (total_time_min - (diena * watch_min))        #Cik minutēs palika kuras nevarēja pārveidot uz pilnām dienām
-reserve_h = leftover_min // 60                               #Atlikušās stundas no rezerves min
-min = leftover_min % 60                                      #Atlikušās min, pēc stundas aprēķina
-    
+# Galvenā programma
+def main():
+    nosaukums, sez = ievaddati()
+    epizodes_skaits = ievadi_ep_skaitu(sez)
+    ep = sum(epizodes_skaits)
+    ep_gar_dic, ep_lenght, watch_min, koef = ievadi_papildus_datus()
+    total_time_min, diena, leftover_min, reserve_h, min = aprekins(ep, ep_gar_dic, ep_lenght, watch_min, koef)
+    izvade(nosaukums, total_time_min, diena, leftover_min, reserve_h, min)
 
-# Izvade
-print(f'Jūs šovu {nosaukums}, skatīsieties: Kopējās min {total_time_min:.0f}, Cik dienas tas aizņems {diena:.0f}, palikušās min {leftover_min:.0f}, Cik pilnu stundu palika {reserve_h:.0f}, cik min palika {min:.0f}')
+if __name__ == "__main__":
+    main()
